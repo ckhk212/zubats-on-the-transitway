77ae1a66d674cad6d1153d3d3584f5e9c46ce9e6
Vincent <vincent.diep61@gmail.com>
2016-09-01 22:55:25 -0400

    Unique substrings function

---
+++ uniqueSubstrings.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function uniqueSubstrings(input) {
+    const hashTable = {};
+    
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j <= input.length; j++) {
+            const str = input.slice(i, j);
+            
+            if (!hashTable[str]) {
+                hashTable[str] = 0;
+            }
+            hashTable[str]++;
+        }
+    }
+    
+    return Object.values(hashTable).filter(value => value === 1).length;
+}
+
+export default uniqueSubstrings;
