a08cb087707bf14598cf1428969d774f3d9d4ab7
Vincent <vincent.diep61@gmail.com>
2016-09-02 14:04:39 -0400

    Array gap sum function

---
+++ arraySumAdjacentDifference/arraySumAdjacentDifference.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function arraySumAdjacentDifference(inputArray) {
+    let s = 0;
+    
+    for (let i = 1; i < inputArray.length; i++) {
+        s += Math.abs(inputArray[i] - inputArray[i - 1]);
+    }
+    
+    return s;
+}
+
+export default arraySumAdjacentDifference;
