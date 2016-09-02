2a80af9c6ef1bc332275b4bcca64b550a060cae3
Vincent <vincent.diep61@gmail.com>
2016-09-02 01:11:07 -0400

    Line encoding function

---
+++ lineEncoding.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function lineEncoding(str) {
+    let length = 0;
+    let buffer = '';
+    let result = '';
+    
+    for (let char of str) {
+        if (length && char !== buffer) {
+            if (length > 1) {
+                result += length;
+            }
+            result += buffer;
+            length = 0;
+        }
+        
+        buffer = char;
+        length++;
+    }
+    
+    if (length > 1) {
+        result += length;
+    }
+    result += buffer;
+    
+    return result;
+}
+
+export default lineEncoding;
