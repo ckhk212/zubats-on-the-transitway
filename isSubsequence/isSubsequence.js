af3c8bc95c0a48d2cc35d02c47e59bddc1c5e810
Vincent <vincent.diep61@gmail.com>
2016-09-16 10:52:11 -0400

    String subsequence function

---
+++ isSubsequence/isSubsequence.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function isSubsequence(haystack, needle) {
+    if (haystack.indexOf(s) > -1) {
+        return true;
+    }
+    
+    let left = 0;
+    for (let char of needle) {
+        if (haystack.slice(left).indexOf(char) === -1) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+export default isSubsequence;
